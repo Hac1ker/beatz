8476041d57ad2484998c26e765948ab4a00236d5
Hac1ker <96375681+Hac1ker@users.noreply.github.com>
2025-04-12 12:05:44 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ function formatTime(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder
-    let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
+    let a = await fetch(`https://hac1ker.github.io/beatz/${folder}/`)
     let response = await a.text()
     // console.log(response)
     let div = document.createElement("div")
@@ -67,7 +67,7 @@ function playmusic(song, pause) {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:3000/songs/`)
+    let a = await fetch(`https://hac1ker.github.io/beatz/songs/`)
     let response = await a.text()
     let div = document.createElement("div")
     div.innerHTML = response
